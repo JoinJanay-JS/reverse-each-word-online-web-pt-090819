637422d43b860b22d72af97127a15a9d880f8d67
Janay Bailey <tjanay87@gmail.com>
2019-09-22 21:25:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,5 @@ require "pry"
 def reverse_each_word(sentence)
   binding.pry
 split_sentence = sentence.split(" ")
-split_sentence.collect do |word|
- word.join(" ") 
-end
+split_sentence.collect {|word| word.reverse}.join(" ") 
 end
