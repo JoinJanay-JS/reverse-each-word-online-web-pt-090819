6e4bf6cbd50183e80a1bd604af81aa699674d318
Janay Bailey <tjanay87@gmail.com>
2019-09-22 21:18:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 def reverse_each_word(sentence)
   binding.pry
 split_sentence = sentence.split(" ")
-split_sentence.collect do
-  reverse.join(" ")
+reverse.join(" ")split_sentence.collect do
+  
 end
 end
